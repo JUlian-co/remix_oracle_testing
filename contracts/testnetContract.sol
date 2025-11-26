cebfd0ca263a36166537349abf9efb15068b6106
JUlian-co <hi@juliansinn.de>
2025-11-26 20:47:17 +0100

    saving changes for today

--- contracts/testnetContract.sol
+++ contracts/testnetContract.sol
@@ -10,7 +10,7 @@ interface IMockPrices {
 }
 
 
-contract PriceOracle {
+contract TestnetPrice {
     address public priceSource;
     
     address public constant token = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238; 
